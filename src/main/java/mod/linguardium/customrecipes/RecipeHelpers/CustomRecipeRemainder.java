9ab69780009c0ab2a3a020b9f0ea67707cd54155
Linguardium <Linguardium@hobbyist.dev>
2020-05-04 22:39:23 -0700

    initial comiit
    implement CustomRecipeRemainder in your Item to set the itemstack it provides back. Allows for recursive remainders (reusable items) as well as items that are damaged per use.

---
+++ src/main/java/mod/linguardium/customrecipes/RecipeHelpers/CustomRecipeRemainder.java
@@ -0,0 +1,8 @@
+package mod.linguardium.customrecipes.RecipeHelpers;
+
+import net.minecraft.item.ItemStack;
+
+public interface CustomRecipeRemainder
+{
+    public ItemStack getRecipeRemainder(ItemStack stack);
+}
